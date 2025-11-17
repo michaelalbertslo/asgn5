4fe9bdbdaee60acb046c13e32c3ab9cb7062aa9f
Ryan Thomas Gertz <rgertz@unix5.csc.calpoly.edu>
2025-11-17 12:17:46 -0800

    header

---
+++ min.h
@@ -0,0 +1,59 @@
+#ifndef MIN_H
+#define MIN_H
+
+#include <stdint.h>
+
+typedef struct __attribute__((packed)) partition_entry {
+  uint8_t  bootind;
+  uint8_t  start_head;
+  uint8_t  start_sec;
+  uint8_t  start_cyl;
+  uint8_t  type;
+  uint8_t  end_head;
+  uint8_t  end_sec;
+  uint8_t  end_cyl;
+  uint32_t lFirst;
+  uint32_t size;
+} partition_entry;
+
+
+typedef struct __attribute__((packed)) superblock {
+  uint32_t ninodes;
+  uint16_t pad1;
+  int16_t  i_blocks;
+  int16_t  z_blocks;
+  uint16_t firstdata;
+  int16_t  log_zone_size;
+  int16_t  pad2;
+  uint32_t max_file;
+  uint32_t zones;
+  int16_t  magic;
+  int16_t  pad3;
+  uint16_t blocksize;
+  uint8_t  subversion;
+} superblock_t;
+
+
+#define DIRECT_ZONES 7
+
+typedef struct __attribute__((packed)) minix_inode {
+  uint16_t mode;
+  uint16_t links;
+  uint16_t uid;
+  uint16_t gid;
+  uint32_t size;
+  int32_t  atime;
+  int32_t  mtime;
+  int32_t  ctime;
+  uint32_t zone[DIRECT_ZONES];
+  uint32_t indirect;
+  uint32_t two_indirect;
+  uint32_t unused;
+} minix_inode;
+
+typedef struct __attribute__((packed)) minix_dirent {
+  uint32_t inode;
+  unsigned char name[60];
+} minix_dirent;
+
+#endif
