2618a1373bcaecb0a0e2a76ccc34ebf5df0ee62e
Ryan Gertz <114454968+RyanGertz@users.noreply.github.com>
2025-11-29 17:59:12 -0800

    Add zone_visit_fn and read_inode function prototypes

--- minfs_common.h
+++ minfs_common.h
@@ -7,6 +7,8 @@
 extern off_t fs_start;
 extern off_t fs_end;
 
+typedef int (*zone_visit_fn)(const zone_span *span, void *user);
+
 void handle_part(FILE *image);
 void handle_superblock(FILE *image, fs_info *fs);
 void init_fs(FILE *image, fs_info *fs, int primary, int subpart);
@@ -14,5 +16,9 @@ void print_superblock(fs_info *fs);
 off_t get_inode_offset(int node_num, fs_info *fs);
 void readinto(void *thing, off_t offset, size_t bytes, FILE *image, size_t *tot);
 void resolve_path(FILE *image, fs_info *fs, minix_inode *inode, char *path);
+int read_inode(FILE *image, fs_info *fs, uint32_t ino, minix_inode *out);
+int iterate_file_zones(FILE *image, fs_info *fs, const minix_inode *in,
+    zone_visit_fn cb, void *user
+);
 
 #endif
